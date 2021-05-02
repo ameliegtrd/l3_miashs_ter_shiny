78a0fcdd7163d09559a9ff8865d4dba45c3cd9f5
Florian-Guinard <79097024+Florian-Guinard@users.noreply.github.com>
2021-05-02 20:02:02 +0200

    Update server.R

--- app2/server.R
+++ app2/server.R
@@ -5,130 +5,239 @@
 
 library(shiny)
 library(shinydashboard)
+library(ggplot2)
 
-
-nb_classes <- function(echantillon,classes){
-  seq(min(echantillon), max(echantillon), length.out = classes +1)
-}
-
-histogramme <- function(echantillon,bins,titre,color = "lightblue", bord = "black",frequence=FALSE){
-  hist(echantillon, breaks = bins, main = titre, col = color, border = bord, freq = frequence)
-}
 
 seq_abs <- function(echantillon,input_echantillon){
-  seq(min(echantillon) - 10, max(echantillon) + 10, length = input_echantillon)
+  seq(min(echantillon) - 0.5, max(echantillon) + 0.5, length = input_echantillon)
 }
 
-
-lignes <- function(x, y, color = "red", line_width = 2){
-  lines(x, y, col = color, lwd = line_width)
-}
-  
 function(input, output){ 
- 
-   # exemple de qu'est-ce que shiny dans l'accueil
+  
+  # exemple de qu'est-ce que shiny dans l'accueil
   output$ex_input <- renderText({
     input$ex_input
-    })
+  })
+  
   
   #loi normale
-  output$dnorm <- renderPlot({
-    ech <-rnorm(input$ech1,input$moyenne,input$ecart_type)
-    # Avant
-    #bins <- seq(min(ech), max(ech), length.out = input$bins +1)
-    #hist(ech, breaks = bins,main = "Histogramme de la loi Normale" ,col = "lightblue", border = "black",freq=FALSE)
-    #lines(grille.x,y,col="red",lwd = 2)
-    bins <- nb_classes(ech, input$bins)
-    histogramme(ech, bins, "Histogramme de la loi Normale")
-    grille.x <- seq_abs(ech,input$ech1)
-    y<-dnorm(grille.x,input$moyenne,input$ecart_type)
-    lignes(grille.x, y)
+  output$dnorm1 <- renderPlot({
+    ech <- rnorm(input$ech1, input$moyenne, input$ecart_type)
+    grille.x <- seq_abs(ech, input$ech1)
+    y<-dnorm(grille.x, input$moyenne, input$ecart_type)
+    df <- cbind(data.frame(ech),data.frame(grille.x),data.frame(y))
+    ggplot(df,aes(ech)) + geom_histogram(aes(y=..density..),bins=input$bins) + 
+      geom_line(aes(x=grille.x,y=y),size=1.5,linetype="dashed",col="red") +
+      labs(title="Fonction de densité",subtitle = "d'une loi normale",x="echantillon",y="densité") + 
+      scale_fill_brewer(palette = "Greens")
+  })
+  
+  output$dnorm2 <- renderPlot({
+    ech <- rnorm(input$ech1, input$moyenne, input$ecart_type)
+    grille.x <- seq_abs(ech, input$ech1)
+    y <- pnorm(grille.x, input$moyenne, input$ecart_type)
+    df <- cbind(data.frame(ech),data.frame(grille.x),data.frame(y))
+    ggplot(df,aes(ech)) + stat_ecdf(geom = "step", col="blue") + 
+      geom_line(aes(x=grille.x, y=y), col="red") +
+      labs(title = "Fonction de répartition d'une loi normale",x="echantillon",y="Fn(x)")
+    
   })
   
   #loi binomiale
   output$dbinom1 <- renderPlot({
     ech <-rbinom(n=input$ech2,size = input$nb_tirages, prob = input$proba)
-    plot(table(ech),main = "Fonction de masse de la loi Binomiale", xlab = "Nombre de succès")
+    grille_x <- min(ech):max(ech)
+    dat <- data.frame(x = grille_x, prob = dbinom(grille_x, size = input$nb_tirages , prob = input$proba))
+    ggplot(dat, aes(x = x, y = prob))+ 
+      geom_segment(aes(xend = x, yend = 0),col="#FC4E07") + 
+      labs(title="Fonction de masse théorique d'une loi Binomiale", x = "Nombre de succes", y = "Proba de succès")
   })
   
   output$dbinom2 <-renderPlot({
     ech <-rbinom(n=input$ech2,size = input$nb_tirages, prob = input$proba)
-    plot(ecdf(ech), main = "Fonction de répartition empirique", cex = 2, col ="red")
+    plot(table(ech)/input$ech2, main = "Fonction de masse empirique de la loi Binomiale",xlab = "Nombre de succes", ylab = "Proba de succès")
+  })
+  
+  output$dbinom3 <-renderPlot({
+    ech <-rbinom(n=input$ech2,size = input$nb_tirages, prob = input$proba)
+    grille.x = seq(min(ech), max(ech),length=500)
+    y = pbinom(grille.x,input$nb_tirages,input$proba)
+    df <- cbind(data.frame(ech),data.frame(grille.x),data.frame(y))
+    ggplot(df, aes(ech)) + stat_ecdf(col="blue") + geom_line(aes(x=grille.x,y=y),col="red") +
+      labs(title = "Fonction de répartition d'une loi Binomiale",x="echantillon",y="Fn(x)")
+    
+    
   })
   
   #loi exponentielle
-  output$dexp <- renderPlot({
-    ech <-rexp(input$ech3,input$lambda2)
-    bins <- nb_classes(ech, input$bins2)
-    histogramme(ech, bins, "Histogramme de la loi Exponentielle")
-    # moche avec cette fonction
-    #grille.x <- seq_abs(ech,input$ech3)
+  output$dexp1 <- renderPlot({
+    ech <- rexp(input$ech3,input$lambda2)
     grille.x <- seq(min(ech),max(ech),length=input$ech3)
-    y<-dexp(grille.x,input$lambda2)
-    lignes(grille.x, y)
+    y <- dexp(grille.x,input$lambda2)
+    df <- cbind(data.frame(ech),data.frame(grille.x),data.frame(y))
+    ggplot(df,aes(ech)) + geom_histogram(aes(y=..density..),bins=input$bins2) + 
+      geom_line(aes(x=grille.x,y=y),size=1.5,linetype="dashed",col="red") +
+      labs(title="Fonction de densité d'une loi Exponentielle",x="echantillon",y="densité") + 
+      scale_fill_brewer(palette = "Greens")
+    
   })
   
+  output$dexp2 <- renderPlot({
+    ech <- rexp(input$ech3,input$lambda2)
+    grille.x <- seq(min(ech),max(ech),length=input$ech3)
+    y <- pexp(grille.x,input$lambda2)
+    df <- cbind(data.frame(ech),data.frame(grille.x),data.frame(y))
+    ggplot(df,aes(ech)) + stat_ecdf(geom = "step", col="blue") + 
+      geom_line(aes(x=grille.x, y=y), col="red") + 
+      labs(title = "Fonction de répartition d'une loi Exponentielle",x="echantillon",y="Fn(x)")
+  })
+  
+  
+ 
   #loi poisson
   output$dpois1 <- renderPlot({
-    ech <-rpois(input$ech4, input$lambda3)
-    plot(table(ech), main = "Fonction de masse de la loi Poisson")
+    ech <- rpois(input$ech4, input$lambda3)
+    grille_x <- min(ech):max(ech)
+    dat <- data.frame(x = grille_x, prob = dpois(grille_x, lambda = input$lambda3))
+    ggplot(dat, aes(x = x, y = prob)) +
+      geom_segment(aes(xend = x, yend = 0),col="red") + 
+      labs(title="Fonction de masse théorique de la loi de Poisson", x = "echantillon", y = "P(x=k)")
+    
+    
   })
   
   output$dpois2 <- renderPlot({
+    ech <- rpois(input$ech4, input$lambda3)
+    plot(table(ech)/input$ech4, main = "Fonction de masse empirique de la loi Poisson")
+    
+    
+  })
+  
+  output$dpois3 <- renderPlot({
     ech <-rpois(input$ech4, input$lambda3)
-    plot(ecdf(ech), cex= 2, col ="red", main = "Fonction de répartition empirique")
+    grille.x = seq(min(ech), max(ech),length=input$ech4)
+    y = ppois(grille.x, input$lambda3)
+    df <- cbind(data.frame(ech),data.frame(grille.x),data.frame(y))
+    ggplot(df, aes(ech)) + stat_ecdf(col="blue") + 
+      geom_line(aes(x=grille.x,y=y),col="red") +
+      labs(title = "Fonction de répartition d'une loi Poisson",x="echantillon",y="Fn(x)")
+    
   })
   
   #loi uniforme  
-  output$dunif <- renderPlot({
+  output$dunif1 <- renderPlot({
     ech <-runif(input$ech5,min(input$range),max(input$range))
-    bins <- nb_classes(ech, input$bins3)
-    histogramme(ech, bins, "Histogramme de la loi Uniforme continue")
     grille.x <- seq_abs(ech,input$ech5)
-    y<-dunif(grille.x,min(input$range),max(input$range))
-    lignes(grille.x, y)
+    y <- dunif(grille.x,min(input$range),max(input$range))
+    df <- cbind(data.frame(ech),data.frame(grille.x),data.frame(y))
+    ggplot(df,aes(ech)) + geom_histogram(aes(y=..density..),bins=input$bins3) + 
+      geom_line(aes(x=grille.x,y=y),size=1.5,linetype="dashed",col="red") +
+      labs(title="Fonction de densité d'une loi Uniforme continue",x="echantillon",y="densité") + 
+      scale_fill_brewer(palette = "Greens")
+  })
+  
+  output$dunif2 <- renderPlot({
+    ech <-runif(input$ech5,min(input$range),max(input$range))
+    grille.x <- seq_abs(ech,input$ech5)
+    y <- punif(grille.x,min(input$range),max(input$range))
+    df <- cbind(data.frame(ech),data.frame(grille.x),data.frame(y))
+    ggplot(df,aes(ech)) + stat_ecdf(geom = "step", col="blue") + 
+      geom_line(aes(x=grille.x, y=y), col="red") +
+      labs(title = "Fonction de répartition d'une loi Uniforme continue",x="echantillon",y="Fn(x)")
   })
   
   #loi gamma
-  output$dgamma <- renderPlot({
+  output$dgamma1 <- renderPlot({
     ech <-rgamma(input$ech6,shape = input$p, scale = input$teta)
-    bins <- nb_classes(ech, input$bins4)
-    histogramme(ech, bins, "Histogramme de la loi Gamma")
     grille.x <- seq_abs(ech,input$ech6)
-    y<-dgamma(grille.x,shape = input$p, scale = input$teta)
-    lignes(grille.x, y)
+    y <- dgamma(grille.x,shape = input$p, scale = input$teta)
+    df <- cbind(data.frame(ech),data.frame(grille.x),data.frame(y))
+    ggplot(df,aes(ech)) + geom_histogram(aes(y=..density..),bins=input$bins4) + 
+      geom_line(aes(x=grille.x,y=y),size=1.5,linetype="dashed",col="red") +
+      labs(title="Fonction de densité d'une loi Gamma",x="echantillon",y="densité") + 
+      scale_fill_brewer(palette = "Greens")
+  })
+  
+  output$dgamma2 <- renderPlot({
+    ech <-rgamma(input$ech6,shape = input$p, scale = input$teta)
+    grille.x <- seq_abs(ech,input$ech6)
+    y <- pgamma(grille.x,shape = input$p, scale = input$teta)
+    df <- cbind(data.frame(ech),data.frame(grille.x),data.frame(y))
+    ggplot(df,aes(ech)) + stat_ecdf(geom = "step", col="blue") + 
+      geom_line(aes(x=grille.x, y=y), col="red") +
+      labs(title = "Fonction de répartition d'une loi Gamma",x="echantillon",y="Fn(x)")
   })
   
   #loi du khi-deux
-  output$dchisq <- renderPlot({
-    ech <-rchisq(input$ech7, df = input$ddl)
-    bins <- nb_classes(ech, input$bins5)
-    histogramme(ech, bins, "Histogramme de la loi du Khi-deux")
+  output$dchisq1 <- renderPlot({
+    ech <- rchisq(input$ech7, df = input$ddl)
     grille.x <- seq_abs(ech,input$ech7)
-    y<-dchisq(grille.x, df = input$ddl)
-    lignes(grille.x, y)
+    y <- dchisq(grille.x, df = input$ddl)
+    df <- cbind(data.frame(ech),data.frame(grille.x),data.frame(y))
+    ggplot(df,aes(ech)) + geom_histogram(aes(y=..density..),bins=input$bins5) + 
+      geom_line(aes(x=grille.x,y=y),size=1.5,linetype="dashed",col="red") +
+      labs(title="Fonction de densité d'une loi Khi-deux",x="echantillon",y="densité") + 
+      scale_fill_brewer(palette = "Greens")
+  })
+  
+  output$dchisq2 <- renderPlot({
+    ech <- rchisq(input$ech7, df = input$ddl)
+    grille.x <- seq_abs(ech,input$ech7)
+    y <- pchisq(grille.x, df = input$ddl)
+    df <- cbind(data.frame(ech),data.frame(grille.x),data.frame(y))
+    ggplot(df,aes(ech)) + stat_ecdf(geom = "step", col="blue") +
+      geom_line(aes(x=grille.x, y=y), col="red") +
+      labs(title = "Fonction de répartition d'une loi Khi-deux",x="echantillon",y="Fn(x)")
   })
   
   #loi student
-  output$dt <- renderPlot({
+  output$dt1 <- renderPlot({
     ech <-rt(input$ech8, df = input$ddl2)
-    bins <- nb_classes(ech, input$bins6)
-    histogramme(ech, bins, "Histogramme de la loi Student")
     grille.x <- seq_abs(ech,input$ech8)
     y <- dt(grille.x, df = input$ddl2)
-    lignes(grille.x, y)
+    df <- cbind(data.frame(ech),data.frame(grille.x),data.frame(y))
+    ggplot(df,aes(ech)) + geom_histogram(aes(y=..density..),bins=input$bins6) + 
+      geom_line(aes(x=grille.x,y=y),size=1.5,linetype="dashed",col="red") +
+      labs(title="Fonction de densité d'une loi Student",x="echantillon",y="densité") + 
+      scale_fill_brewer(palette = "Greens")
+    
   })
   
+  output$dt2 <- renderPlot({
+    ech <-rt(input$ech8, df = input$ddl2)
+    grille.x <- seq_abs(ech,input$ech8)
+    y <- pt(grille.x, df = input$ddl2)
+    df <- cbind(data.frame(ech),data.frame(grille.x),data.frame(y))
+    ggplot(df,aes(ech)) + stat_ecdf(geom = "step", col="blue") + 
+      geom_line(aes(x=grille.x, y=y), col="red") +
+      labs(title = "Fonction de répartition d'une loi Student",x="echantillon",y="Fn(x)")
+  })
+  
+  
   #loi Fisher
-  output$df <- renderPlot({
+  output$df1 <- renderPlot({
     ech <-rf(input$ech9, df1 = input$ddl3, df2 = input$ddl4)
-    bins <- nb_classes(ech, input$bins7)
-    histogramme(ech, bins, "Histogramme de la loi de Fisher")
     grille.x <- seq_abs(ech,input$ech9)
     y<-df(grille.x, df1 = input$ddl3, df2 = input$ddl4)
-    lignes(grille.x, y)
+    df <- cbind(data.frame(ech),data.frame(grille.x),data.frame(y))
+    ggplot(df,aes(ech)) + geom_histogram(aes(y=..density..),bins=input$bins7) + 
+      geom_line(aes(x=grille.x,y=y),size=1.5,linetype="dashed",col="red") +
+      labs(title="Fonction de densité d'une loi Fisher",x="echantillon",y="densité") + 
+      scale_fill_brewer(palette = "Greens")
+    })
+  
+  output$df2 <- renderPlot({
+    ech <-rf(input$ech9, df1 = input$ddl3, df2 = input$ddl4)
+    grille.x <- seq_abs(ech,input$ech9)
+    y<-pf(grille.x, df1 = input$ddl3, df2 = input$ddl4)
+    df <- cbind(data.frame(ech),data.frame(grille.x),data.frame(y))
+    ggplot(df,aes(ech)) + stat_ecdf(geom = "step", col="blue") + 
+      geom_line(aes(x=grille.x, y=y), col="red") +
+      labs(title = "Fonction de répartition d'une loi Fisher",x="echantillon",y="Fn(x)")
   })
 }
+
+
 
 
 
